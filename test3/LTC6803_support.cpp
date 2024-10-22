91d7dcab60917e9e00aba040e8a403c2a879f124
gregclacker <yongboone1@gmail.com>
2024-10-21 22:25:36 -0500

    corrected comments on the crc process

--- test3/LTC6803_support.cpp
+++ test3/LTC6803_support.cpp
@@ -10,41 +10,27 @@ uint16_t ADCRangeTomV(uint16_t adc) {
 /** 
  * calculates a 8 bit pec code from byte array using the datasheets process (ltc.17).
  * 
- * ltc uses a pec quotent ("PEC_POLY") of 0x7, with a seed of 0x41.
- *      - i think its just a 'x^8,2,1,-1 + 65' polynomial when u consider how their funciton 
- *          works.
+ * ltc defines a polynomial ("PEC_POLY") of 0x7, with a seed of 0x41.
+ *      - effectively acts as a 'x^8,2,1 + 1' polynomial, with a seed of 0x41
  *      LT paid them to pull that number out of their _ + im not 
  *          => I trust them (aka I have no idea why they chose it).
 */
 uint8_t pec8calc(uint8_t len, uint8_t* data) {
-    //see ltc.17 for step numbers & variable names
+    //see ltc.17
     // not strictly following because its giving like a hardware bit-by-bit process
     //  thats not really viable for programming languages
+    // we know hte poly anyways, going to ignore their process
 
-    // #1
     uint8_t remainder = 0x41; 
 
-    // #2
     for (uint8_t din = 0; din < len; ++din) {
         remainder ^= data[din];
 
-        //the in0, in1, in2 stuff simplifies down to a slightly modded crc operation
-        //  with the caveat that shift first then subtract.
-        //  notice they dont actually tell us the quotent ( :( ) but its something 
-        //  like 0x7, if you just look how its in0,1,2 (first 3 bits) being flipped,
-        //  after the shift, so just '0x7 >> 1'. im missing somethign since I cant 
-        //  explain the choice of shift first then subtract. I think this is either 
-        //  a 4d power play going on for IP laws some evil critter thought up, or 
-        //  just a way to detect 8 bit bursts within a crc-8 bit limit, since its 
-        //  aligning the 8th bit while also factoring the -1th bit.
-        // #LTHadItCommingForBadDocumentation
-
         for (uint8_t bit = 8; bit > 0; --bit) {
 
-            // #2 & #3
             if (remainder & (1<<7))
                 remainder = (remainder << 1) ^ 0x7;
-            else // #3
+            else
                 remainder <<= 1;
         }
 
@@ -54,6 +40,7 @@ uint8_t pec8calc(uint8_t len, uint8_t* data) {
 }
 
 /**
+ * DOES NOT WORK, i forgo to account for the offset. i ll fix it later
  * general purpose pec8 calculator function.
  *  does not do width extension.
  * 
